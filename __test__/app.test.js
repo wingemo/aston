9ec8dd80647c12e32ccdb6d00218e52e6fe57354
Philip Wingemo <philip.wingemo@outlook.com>
2023-07-10 21:09:00 +0200

    v.2

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -2,12 +2,10 @@ const axios = require('axios');
 
 const url = 'http://127.0.0.1:3000';
 
-describe('The router', () => {
-  test('The get route', async () => {
+describe('Server Status', () => {
+  test('Server Running', async () => {
     const res = await axios.get(url)
-
     expect(res).toBeTruthy()
     expect(res.status).toBe(200)
-    expect(res.data).toEqual('Hello World!')
   })
 })
