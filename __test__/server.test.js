d780a47be0d17745e0c4d9082a939724426972b2
Philip Wingemo <philip.wingemo@outlook.com>
2023-07-11 10:20:15 +0200

    Future Router Object

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -2,10 +2,11 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
 
-describe('Server Status', () => {
-  test('Server Running', async () => {
+describe('Server', () => {
+  test('Server Status', async () => {
     const res = await axios.get(process.env.URL)
     expect(res).toBeTruthy()
     expect(res.status).toBe(200)
   })
 })
+
